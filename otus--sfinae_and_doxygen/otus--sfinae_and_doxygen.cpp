acedc825d6069323e1bbaea4448812eb5c45ecbf
SeresHotes <sereshotes@gmail.com>
2021-11-30 11:37:39 +0300

    Добавьте файлы проекта.

---
+++ otus--sfinae_and_doxygen/otus--sfinae_and_doxygen.cpp
@@ -0,0 +1,12 @@
+﻿// otus--sfinae_and_doxygen.cpp: определяет точку входа для приложения.
+//
+
+#include "otus--sfinae_and_doxygen.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
